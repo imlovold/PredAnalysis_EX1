e7a05a380f660a56a19084a6d65fb1bf5b12d5fd
theaboge <theabogee@gmail.com>
2025-08-26 14:39:34 -0300

    edit name

--- streamlit_app_.py
+++ streamlit_app_.py
@@ -8,7 +8,7 @@ def load_data():
 data = load_data()
 
 st.title("Heart Disease Dataset Dashboard")
-st.write("Use the sidebar to explore metadata, categorical distributions, correlations, and more.")
+st.write("Use the sidebar to explore metadata, distributions, correlations, and more.")
 
 st.write("### Dataset Quick Info")
 st.write(f"Rows: {data.shape[0]}, Columns: {data.shape[1]}")
